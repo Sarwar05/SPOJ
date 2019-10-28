2666b3aee4a2960b136cf1e44a8f74c93ff66bd0
Sarwar Jahan <Sarwar05@users.noreply.github.com>
2019-10-28 15:13:02 +0600

    Add files via upload

--- NAJPF - Pattern Find.cpp
+++ NAJPF - Pattern Find.cpp
@@ -1,38 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int mx = 1e6+6;
-char text[mx], pattern[mx];
-#define ll long long
-const ll prime = 31;
-const ll mod = 1e9+7;
-ll Pow[mx], Hash[mx];
+vector<int> prefix(string& str)
+{
+    int n = str.size();
+    vector<int> pi(n,0);
+    for(int i=1; i<n; i++){
+        int j = pi[i-1];
+        while(j>0 && str[i]!=str[j])
+            j = pi[j-1];
+        if(str[i]==str[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
 int main()
 {
-    Pow[0] = 1;
-    for(int i=1; i<mx; i++)
-        Pow[i] = (Pow[i-1]*prime)%mod;
     int tc;
     scanf("%d",&tc);
+    string s,t;
     while(tc--){
-        scanf("%s%s",text, pattern);
-        int len1 = strlen(text);
-        int len2 = strlen(pattern);
-        ll val = 0;
-        for(int i=0; i<len1; i++){
-            val = (val + text[i]*Pow[i])%mod;
-            Hash[i] = val;
-        }
-        val = 0;
-        for(int i=0; i<len2; i++)
-            val = (val + pattern[i]*Pow[i])%mod;
-
+        cin>>s>>t;
+        int n = t.size();
+        s = (t+"#"+s);
+        vector<int> pref = prefix(s);
         vector<int> ans;
-        for(int i = 0, j = len2-1; j<len1; i++, j++){
-            ll now = Hash[j];
-            if(i) now-=Hash[i-1];
-            if(now<0) now+=mod;
-            if((val*Pow[i])%mod == now)
-                ans.push_back(i+1);
+        for(int i=n+1; i<s.size(); i++){
+            if(pref[i]==t.size())
+                ans.push_back(i-2*n+1);
         }
         int sz = ans.size();
         if(sz==0)printf("Not Found\n");
